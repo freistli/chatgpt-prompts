0fed18bbb5c20cc0d16a7a1742ce4de7d1137a3e
freistli <freistli@microsoft.com>
2023-03-31 10:41:09 +0800

    handle prompt+msgid

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,8 +8,15 @@ export const createPromptFactory = (instance: AzureChatGPTAPI, prompt: string) =
       res = await instance.sendMessage(prompt);
       messageId= res.id;
     }
+    else 
+    {
+      if(prompt)
+      res = await instance.sendMessage(prompt, {
+        parentMessageId: messageId
+      });
+      messageId= res.id;
+    }
     
-
     res = await instance.sendMessage(message, {
       parentMessageId: messageId
     });
